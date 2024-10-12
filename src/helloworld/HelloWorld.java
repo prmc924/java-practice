f2f39664ffd7dcabb617b7374fbf775e3af21b30
prmc924 <prcm924@gmail.com>
2024-10-12 16:10:44 +0100

    Java-practice project created

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
