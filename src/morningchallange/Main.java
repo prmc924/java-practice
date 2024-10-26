b3920ba6a8352a8fe83cd32ccfe79a73e0889338
prmc924 <prcm924@gmail.com>
2024-10-26 14:29:37 +0100

    Challenge3 and inheritance and abstraction example

---
+++ src/morningchallange/Main.java
@@ -0,0 +1,15 @@
+package morningchallange;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Challenge3 emp1 = new Challenge3(101, 10,"John", "Manual Tester");
+        emp1.display();
+        Challenge3 emp2 = new Challenge3(102, 12, "Smith", "Automation Tester");
+        emp2.display();
+        emp1.display();
+    }
+}
